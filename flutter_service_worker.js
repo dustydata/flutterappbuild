b3168ad0160ac84570669570d8b9f3d81b4c1a48
xpat <cheftech@pinchepoutine.digital>
2020-11-05 16:14:40 -0500

    post Provider.of commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,19 +3,19 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "9823b769cbc11a929ef739ca4e9bc6eb",
+  "main.dart.js": "ddcf81be87379b477e474f31010f7502",
 "version.json": "8ff4b987c5b35f29ca4df6965a4e3c1b",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/NOTICES": "43777e8fdf862b5fd604d5ea0a5f10af",
+"assets/NOTICES": "05b0640fbdcfa88ed59e18575c739fc5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "d03e2ebb2d91c03ed93bda65c08e2f20",
-"index.html": "2e3f4e45a3d540cec266ac053a7d3b15",
-"/": "2e3f4e45a3d540cec266ac053a7d3b15"
+"index.html": "5ca2d00e8e36798fcb448895ffdb0fdf",
+"/": "5ca2d00e8e36798fcb448895ffdb0fdf"
 };
 
 // The application shell files that are downloaded before a service worker can
